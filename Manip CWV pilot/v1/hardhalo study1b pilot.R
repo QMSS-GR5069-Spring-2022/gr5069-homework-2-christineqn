99088dce8dcf06245f4e20108e6fa7cfed09976d
yizhenglu <lucasjob916@gmail.com>
2022-03-22 13:11:30 -0400

    formated R file

--- Manip CWV pilot/v1/hardhalo study1b pilot.R
+++ Manip CWV pilot/v1/hardhalo study1b pilot.R
@@ -2,7 +2,7 @@
 title: "hardhalo study1b pilot-V1"
 author: "Christine Nguyen "
 Date: 
-R Studio Version:
+RStudio_Version:
 ---
 
 setwd('C:/Users/c1/OneDrive/Desktop/Fall 2021/Hard halos F21/Manip CWV pilot/v1')
@@ -14,28 +14,31 @@ library(psych)
 # PREPROCESSING ########################################
 
 # read in data ####
-study1b_data <- read.csv('hardhalo study1b pilot_num_012622.csv')
-study1b_data <- study1b_data %>% filter(Status==0) #remove non-data rows (variable info + test rows)
+study1b_data <- read.csv("hardhalo study1b pilot_num_012622.csv")
+study1b_data <- study1b_data %>%
+  filter(Status == 0)  #remove non-data rows (variable info + test rows)
 
 # rename ####
-varnames <- read_xlsx('../../var names.xlsx', sheet='study1b')
-oldnames <- varnames[!is.na(varnames$old),]$old #removing empty rows
-newnames <- varnames[!is.na(varnames$new),]$new #removing empty rows
-study1b_data <- study1b_data %>% 
+varnames <- read_xlsx("../../var names.xlsx", sheet = "study1b")
+oldnames <- varnames[!is.na(varnames$old), ]$old  #removing empty rows
+newnames <- varnames[!is.na(varnames$new), ]$new  #removing empty rows
+study1b_data <- study1b_data %>%
   rename_at(vars(all_of(oldnames)), ~all_of(newnames))
 # remove randomization order variables
 study1b_data <- study1b_data %>%
-  select(-ends_with('_DO', ignore.case=F))
+  select(-ends_with("_DO", ignore.case = F))
 
 # make numeric vars numeric ####
-check_numeric <- function(x) { return(!is.na(as.numeric(x[1]))) }
+check_numeric <- function(x) {
+  return(!is.na(as.numeric(x[1])))
+}
 study1b_data <- study1b_data %>%
   mutate_if(check_numeric, as.numeric)
 
 # combine open-ended columns for two conditions
 study1b_data <- study1b_data %>%
-  tidyr::unite(CWVopen, highCWVopen, lowCWVopen, sep='') %>%
-  tidyr::unite(CWVactopen, highCWVactopen, lowCWVactopen, sep='')
+  tidyr::unite(CWVopen, highCWVopen, lowCWVopen, sep = "") %>%
+  tidyr::unite(CWVactopen, highCWVactopen, lowCWVactopen, sep = "")
 
 # drop incomplete cases ####
 # sum(study1b_data$Finished!=1) #0
@@ -43,7 +46,7 @@ study1b_data <- study1b_data %>%
 
 # drop cases that fail the attention check ####
 study1b_data <- study1b_data %>%
-  filter(entered_mTurkID==workerId & attencheck_1==2) #Reordered the structures to make the code more clear 
+  filter(entered_mTurkID == workerId & attencheck_1 == 2)  #Reordered the structures to make the code more clear  
 # nrow(study1b_data) #145
 
 # calculate mean CWV ####
@@ -59,8 +62,8 @@ study1b_data <- study1b_data %>%
 
 # factorizing random condition ####
 study1b_data <- study1b_data %>%
-  mutate(CWVmanip = ifelse(CWVmanip=='CWVmaniplow',0,1),
-         targetgender = ifelse(target=='Jennifer',2,1))
+  mutate(CWVmanip = ifelse(CWVmanip == "CWVmaniplow", 0, 1), targetgender = ifelse(target ==
+                                                                                     "Jennifer", 2, 1))
 
 # check randomization ###
 # study1b_data %>%
@@ -77,9 +80,10 @@ study1b_data <- study1b_data %>%
 #       check.keys=T) #0.
 
 
-study1b_data <- study1b_data %>% rowwise() %>%
-  mutate(gen_competent = mean(c(competent, intelligent)),
-         org_competent = mean(c(leader, negotiator, solver))) %>%
+study1b_data <- study1b_data %>%
+  rowwise() %>%
+  mutate(gen_competent = mean(c(competent, intelligent)), org_competent = mean(c(leader,
+                                                                                 negotiator, solver))) %>%
   ungroup()
 
 # other constructs ####
@@ -96,30 +100,33 @@ study1b_data <- study1b_data %>% rowwise() %>%
 
 
 
-study1b_data <- study1b_data %>% rowwise() %>%
+study1b_data <- study1b_data %>%
+  rowwise() %>%
   mutate(posTIPI = mean(c(open, dependable, extravert, warm, calm)),
-         genimpression = mean(c(pos_genimpression, 6-neg_genimpression)),
-         adapt = mean(c(outcome, motivation, backfire, performance)),
-         self_bell = mean(c(self_dominant, self_forceful, self_cold, 6-self_warm))) %>%
+         genimpression = mean(c(pos_genimpression, 6 - neg_genimpression)),
+         adapt = mean(c(outcome, motivation, backfire, performance)), self_bell = mean(c(self_dominant,
+                                                                                         self_forceful, self_cold, 6 - self_warm))) %>%
   ungroup()
 
 # categorical self_bell ####
-lowselfbell <- mean(study1b_data$self_bell)-sd(study1b_data$self_bell) #2.56
-highselfbell <- mean(study1b_data$self_bell)+sd(study1b_data$self_bell) #3.96
+lowselfbell <- mean(study1b_data$self_bell) - sd(study1b_data$self_bell)  #2.56
+highselfbell <- mean(study1b_data$self_bell) + sd(study1b_data$self_bell)  #3.96
 study1b_data <- study1b_data %>%
-  mutate(cat_selfbell = factor(ifelse(self_bell<lowselfbell,'low',
-                                 ifelse(self_bell>=highselfbell,'high','med')),
-                          levels=c('low','med','high')))
+  mutate(cat_selfbell = factor(ifelse(self_bell < lowselfbell, "low",
+                                      ifelse(self_bell >= highselfbell, "high", "med")), levels = c("low",
+                                                                                                    "med", "high")))
 
 
 # write.csv(study1b_data, file='study1b_processed.csv', row.names=F)
-study1b_data <- read.csv('study1b_processed.csv') %>%
-  mutate(cat_selfbell = factor(cat_selfbell, levels=c('low','med','high')))
+study1b_data <- read.csv("study1b_processed.csv") %>%
+  mutate(cat_selfbell = factor(cat_selfbell, levels = c("low", "med",
+                                                        "high")))
 
-study1b_onlymed <- study1b_data %>% filter(cat_selfbell == 'med')
+study1b_onlymed <- study1b_data %>%
+  filter(cat_selfbell == "med")
 
-study1b_onlyqual <- read_xlsx('study1b_processed.xlsx') %>%
-  filter(screenopen==0) # screen garbage manipulation open responses
+study1b_onlyqual <- read_xlsx("study1b_processed.xlsx") %>%
+  filter(screenopen == 0)  # screen garbage manipulation open responses
 
 
 # nrow(study1b_onlyqual) #76
@@ -133,8 +140,10 @@ study1b_onlyqual <- read_xlsx('study1b_processed.xlsx') %>%
 proportions(table(study1b_data$race))
 proportions(table(study1b_data$gender))
 proportions(table(study1b_data$education))
-mean(study1b_data$age[study1b_data$age<150]); sd(study1b_data$age[study1b_data$age<150])
+mean(study1b_data$age[study1b_data$age < 150])
+sd(study1b_data$age[study1b_data$age < 150])
 proportions(table(study1b_data$working))
+
 
 # manipulation check ####
 summary(lm(CWV ~ CWVmanip, study1b_data))
